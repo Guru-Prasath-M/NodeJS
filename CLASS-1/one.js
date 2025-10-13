18a588f66401c54041b823b94832ac3a0c7ec68f
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-10-13 18:38:07 +0530

    Introduction

---
+++ CLASS-1/one.js
@@ -0,0 +1,6 @@
+//ES-5
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.arch());
---
+++ CLASS-1/two.js
@@ -0,0 +1,6 @@
+//ES-6
+import os from 'os'
+
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.homedir())
